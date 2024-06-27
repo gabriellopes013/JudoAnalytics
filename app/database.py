a0033441040289fc6f058f783dbfb8a12462e8d6
Judo <judo@MacBook-Air-de-Gabriel.local>
2024-06-27 11:39:54 -0300

    Adicionando df_teste

--- app/database.py
+++ app/database.py
@@ -44,7 +44,6 @@ def insert_atleta(genero, nome, sobrenome, ctg_idade, ctg_peso, clube):
         VALUES (?, ?, ?, ?, ?, ?)
     """, (genero, nome, sobrenome, ctg_idade, ctg_peso, clube))
     con.commit()
-    con.close()
 
 def insert_luta(id_atleta, adversario, minuto_luta, dir_golpe, postura):
     con.execute("""
@@ -52,7 +51,6 @@ def insert_luta(id_atleta, adversario, minuto_luta, dir_golpe, postura):
         VALUES (?, ?, ?, ?, ?)
     """, (id_atleta, adversario, minuto_luta, dir_golpe, postura))
     con.commit()
-    con.close()
 
 # Função para obter todos os dados
 def get_atletas():
@@ -65,13 +63,14 @@ def check_existing_atleta(atleta_id):
 def get_lutas():
     return con.execute("SELECT * FROM lutas").fetchdf()
 def df_teste():
+    con = duckdb.connect('judo_app.db')
+
     query = """
-    SELECT id, nome, sobrenome, idade, categoria, clube
+    SELECT id, nome, sobrenome, ctg_idade, ctg_peso, clube
     FROM atletas
     """
     df_atletas = pd.read_sql(query, con)
 
-    con.close()
     return df_atletas
 
 
