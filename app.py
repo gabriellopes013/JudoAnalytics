c3333e30383c3c6abeaae6bc18ac4ee9b88e61d9
Judo <judo@MacBook-Air-de-Gabriel.local>
2024-06-27 22:46:07 -0300

    Alterando app.py

--- app.py
+++ app.py
@@ -1,17 +1,18 @@
 import streamlit as st
-from app.atletas import register_athletes
-from app.database import df_teste
+# from app.atletas import register_athletes
+# from app.database import df_teste
 # from register_fights import register_fights
 def main():
     st.sidebar.title('Navegação')
     page = st.sidebar.radio('Ir para', ['Registrar Atletas', 'Registrar Lutas'])
+    st.write("Deu certo")
 
-    if page == 'Registrar Atletas':
-        register_athletes()
-    # elif page == 'Registrar Lutas':
-    #     register_fights()
-    df_atletas = df_teste()
+    # if page == 'Registrar Atletas':
+    #     register_athletes()
+    # # elif page == 'Registrar Lutas':
+    # #     register_fights()
+    # df_atletas = df_teste()
 
-    st.table(df_atletas)
+    # st.table(df_atletas)
 if __name__ == '__main__':
     main()
